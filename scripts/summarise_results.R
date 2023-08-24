5988215508041e9c71a2d35d750a4f4336ca6500
n-kall <n-kall@github.com>
2023-08-24 12:39:32 +0300

    Only read csv files in summary script

--- scripts/summarise_results.R
+++ scripts/summarise_results.R
@@ -2,7 +2,7 @@ library(vroom)
 results_path <- snakemake@config[["results_path"]]
 summary_path <- snakemake@config[["summary_path"]]
 
-files <- list.files(results_path)
+files <- list.files(results_path, pattern = "*.csv")
 
 out <- vroom(paste0(results_path, files))
 
